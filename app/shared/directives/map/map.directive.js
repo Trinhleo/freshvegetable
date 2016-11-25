7001180c6088fd04f038850fef229689ac8faf5b
Trinhleo <hnkt2907@gmail.com>
2016-11-26 03:29:55 +0700

    ss

--- app/shared/directives/map/map.directive.js
+++ app/shared/directives/map/map.directive.js
@@ -87,7 +87,7 @@
             function addMapScript() {
                 var script = document.createElement('script');
                 script.type = 'text/javascript';
-                script.src = "https://maps.googleapis.com/maps/api/js?callback=initialize"
+                script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBBTkhMKJ0kR0iPfFaoLNq1BgWZyoeJrMA&&callback=initialize"
                 document.body.appendChild(script);
             };
 
